271cf155f8b69e932b967956748eee386c09e921
Sebattaz <seba.lostar@gmail.com>
2024-07-12 21:14:45 -0400

    View home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Container, Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+    const go = useNavigate();
+
+    const aLaAcccion=()=>{
+      go('/pokemones')
+    }
   return (
-    <div>
-      HOME
-    </div>
+    <Container className='d-flex flex-column justify-content-center align-items-center  w-50 h-75' >
+      <h1>¡Bienvenido!</h1>
+      <h2>¡Maestro Pokemon!</h2>
+      <img src="./src/img/Pikachu.png" alt="Pikachu" className='w-50 h-75'/>
+      <Button onClick={aLaAcccion} className='mt-5'>!A la accion!</Button>
+    </Container>
   )
 }
 
